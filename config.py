0ab76625d556cfd9a83d89e66c5814cfd9b0cdd5
yba9285 <108516941+yba9285@users.noreply.github.com>
2026-04-07 14:04:36 +0530

    Rename database name from 'Vlz-Filestore' to 'VlzFilestore'

--- config.py
+++ config.py
@@ -20,7 +20,7 @@ API_HASH = "" # API HASH
 WORKERS = 5
 
 DB_URI = "" # MongoDB URI
-DB_NAME = "Vlz-Filestore"
+DB_NAME = "VlzFilestore"
 
 FSUBS = [[-1001931113198, True, 10]] # Force Subscription Channels [channel_id, request_enabled, timer_in_minutes]
 # Database Channel (Primary)
